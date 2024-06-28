e75620bc2e2aabbd8b2b6e9c8cb1739488819d17
Vadim Zhuravlev <vadim.zhuravlev120498@mail.ru>
2024-06-28 09:35:57 +0300

    add Code

---
+++ src/test/java/ru/netology/task/TaskTest.java
@@ -0,0 +1,41 @@
+package ru.netology.task;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TaskTest {
+    SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+    String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+    Epic epic = new Epic(55, subtasks);
+
+    Meeting meeting = new Meeting(
+            555,
+            "Выкатка 3й версии приложения",
+            "Приложение НетоБанка",
+            "Во вторник после обеда"
+    );
+
+    @Test
+    public void giveMeAllTasks() {
+        boolean searchSimple = simpleTask.matches("Позвонить родителям");
+        boolean searchEpic = epic.matches("Хлеб");
+        boolean searchMeeting = meeting.matches("Приложение НетоБанка");
+
+        Assertions.assertTrue(searchSimple);
+        Assertions.assertTrue(searchEpic);
+        Assertions.assertTrue(searchMeeting);
+    }
+
+
+    @Test
+    public void dontGiveMeTasksAllTasks() {
+        boolean searchSimple = simpleTask.matches("Позвонить брату");
+        boolean searchEpic = epic.matches("Соль");
+        boolean searchMeeting = meeting.matches("Приложение НетБанка");
+
+        Assertions.assertFalse(searchSimple);
+        Assertions.assertFalse(searchEpic);
+        Assertions.assertFalse(searchMeeting);
+    }
+
+}
---
+++ src/test/java/ru/netology/task/TodosTest.java
@@ -0,0 +1,72 @@
+package ru.netology.task;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TodosTest {
+    SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+    String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+    Epic epic = new Epic(55, subtasks);
+
+    Meeting meeting = new Meeting(
+            555,
+            "Выкатка 3й версии приложения",
+            "Приложение НетоБанка",
+            "Во вторник после обеда"
+    );
+
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+        Todos todos = new Todos();
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void giveMeATaskWithSimpleTask() {
+
+        Todos search = new Todos();
+        search.add(simpleTask);
+        search.add(epic);
+        search.add(meeting);
+
+        Task[] expected = {simpleTask};
+        Task[] actual = search.search("Позвонить родителям");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void giveMeSearchAllTasks() {
+        Todos search = new Todos();
+        search.add(simpleTask);
+        search.add(epic);
+        search.add(meeting);
+
+        Task[] actual1 = search.search("Позвонить родителям");
+        Task[] actual2 = search.search("Яйца");
+        Task[] actual3 = search.search("Приложение НетоБанка");
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[][] actual = {actual1, actual2, actual3};
+    }
+
+    @Test
+    public void giveMeSearchNoteTasks() {
+        Todos search = new Todos();
+        search.add(simpleTask);
+        search.add(epic);
+        search.add(meeting);
+
+        Task[] expected = {};
+        Task[] actual = search.search("Вода");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
